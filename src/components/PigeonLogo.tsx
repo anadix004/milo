facb81d88461923b0a674158337b4a05fc7e65d7
anadix004 <anadix004@gmail.com>
2026-04-03 13:19:49 +0530

    feat: implement Signature Pigeon Hub logo (Iconic & Understandable Edition)

--- src/components/PigeonLogo.tsx
+++ src/components/PigeonLogo.tsx
@@ -17,39 +17,22 @@ export default function PigeonLogo({ className, size = 24, animate = true }: Pig
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
-      whileHover={{ scale: 1.05 }}
+      whileHover={{ scale: 1.1 }}
     >
       {/* Cinematic Orbit Ring */}
-      <circle cx="50" cy="50" r="46" stroke="white" strokeWidth="2.5" opacity="0.9" />
+      <circle cx="50" cy="50" r="46" stroke="white" strokeWidth="2" opacity="0.3" />
 
-      {/* Subtle Stardust (4-pointed stars) */}
-      <g opacity="0.7">
-        <motion.path 
-          d="M32 30L34 32L32 34L30 32L32 30Z" 
-          fill="white" 
-          animate={{ opacity: [0.3, 0.8, 0.3], scale: [0.8, 1.2, 0.8] }}
-          transition={{ duration: 2, repeat: Infinity }}
-        />
-        <motion.path 
-          d="M68 25L70 27L68 29L66 27L68 25Z" 
-          fill="white" 
-          animate={{ opacity: [0.4, 0.9, 0.4], scale: [0.9, 1.3, 0.9] }}
-          transition={{ duration: 2, repeat: Infinity, delay: 0.5 }}
-        />
-        <motion.path 
-          d="M35 68L37 70L35 72L33 70L35 68Z" 
-          fill="white" 
-          animate={{ opacity: [0.2, 0.7, 0.2], scale: [0.7, 1.1, 0.7] }}
-          transition={{ duration: 2, repeat: Infinity, delay: 1 }}
-        />
-      </g>
-
-      {/* Master Flying Pigeon Silhouette (High Fidelity Refined) */}
-      <path
-        d="M72 48.5C68 46 62 44 58 46C54 48 51 51 50 55C48.5 60 49 66 51 71C48 69 45.5 65 44 61C41 53 41.5 44 45 37C48.5 30 55 24 62 21C52 25 42 33 38 44C34 55 36 67 43 77C36 70 32 61 32 51C32 41 36 30 43 21C23 31 10 51 10 74C12 67 16 61 23 57C30 53 38 54 46 57C36 54 26 54 16 57C14 71 16 84 23 91C16 83 12 73 10 63C6 61 0 61 -6 63L-8 61L-6 59C-3 58 0 58 3 59C6 58 9 56 12 54C19 50 28 47 38 47C48 47 58 50 64 54L72 48.5Z"
+      {/* Iconic Pigeon Silhouette (Understandable & Professional) */}
+      <motion.path
+        d="M68 38C66 36 62 35 59 36C56 37 54 39 53 43C52.5 47 53 52 55 57C52 55 49.5 52 48 49C45 42 45.5 35 48 29C50.5 23 56 18 63 15C53 18 45 25 41 34C37 45 38 56 45 66C38 59 34 51 34 42C34 33 38 24 45 16C25 26 12 46 12 70C15 63 18 57 25 53C32 49 40 49 48 52C38 49 28 49 18 52C16 66 18 78 25 85C18 77 14 68 12 58C8 56 3 56 -3 58L-5 56L-3 54C0 53 3 53 6 54C9 52 12 50 15 48C22 44 31 41 41 41C51 41 61 44 68 49L72 44C74 42 77 41 80 41C83 41 85 43 85 46C85 49 84 51 82 52L68 38Z"
         fill="white"
-        transform="translate(10, 0) scale(0.9)"
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
       />
+      
+      {/* Subtle Tail Fan Detail */}
+      <path d="M22 80L18 84M26 82L24 86" stroke="white" strokeWidth="1" strokeLinecap="round" opacity="0.2" />
     </motion.svg>
   );
 }
